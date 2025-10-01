a71192e73e4d52c14bedb5bb18984dcbae9b9817
Jegadish Velayutham <jegadishvelayutham@Jegadishs-MacBook-Air.local>
2025-10-01 12:26:10 +0530

    added files

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 
+from opentelemetry import metrics
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from prometheus_client import Counter, Histogram
@@ -39,7 +45,7 @@ resource = Resource.create(attributes={"service.name": "task-tracker"})
 
 # ------- Tracing -------
 trace_provider = TracerProvider(resource=resource)
-trace_exporter = OTLPSpanExporter(endpoint="http://fastapi-otel-collector-1:4317", insecure=True)
+trace_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4317", insecure=True)
 trace_provider.add_span_processor(BatchSpanProcessor(trace_exporter))
 trace.set_tracer_provider(trace_provider)
 tracer = trace.get_tracer(__name__)
@@ -47,14 +53,19 @@ tracer = trace.get_tracer(__name__)
 # ------- Logging -------
 logger_provider = LoggerProvider(resource=resource)
 set_logger_provider(logger_provider)
-log_exporter = OTLPLogExporter(endpoint="http://fastapi-otel-collector-1:4317", insecure=True)
+log_exporter = OTLPLogExporter(endpoint="http://otel-collector:4317", insecure=True)
 logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
 handler = LoggingHandler(logger_provider=logger_provider)
 logging.getLogger().addHandler(handler)
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
-
+#---------- Metrics -----------
+metric_exporter = OTLPMetricExporter(endpoint="http://otel-collector:4317", insecure=True)
+metric_reader = PeriodicExportingMetricReader(metric_exporter)
+meter_provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
+metrics.set_meter_provider(meter_provider)
+meter = metrics.get_meter(__name__)
 # Database configuration
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/postgres")
 database = databases.Database(DATABASE_URL)
@@ -156,6 +167,16 @@ async def get_task(task_id: int):
         logger.info(f"Fetched task: id={task['id']}, title={task['title']}, description={task['description']}, completed={task['completed']}, created_at={task['created_at']}, updated_at={task['updated_at']}") 
     return task
 
+@app.delete("/tasks/{task_id}", status_code=204)
+async def delete_task(task_id: int):
+    with tracer.start_as_current_span("delete_task"):
+        query = tasks.delete().where(tasks.c.id == task_id)
+        result = await database.execute(query)
+        if result == 0:
+            logger.warning(f"Task not found to delete with id: {task_id}")
+            raise HTTPException(status_code=404, detail="Task not found")
+        logger.info(f"Deleted task with id: {task_id}")
+        return Response(status_code=204)
 
 @app.middleware("http")
 async def prometheus_middleware(request, call_next):
