9cd63987145a71ae068ddec2b52588c47662f548
Jegadish Velayutham <jegadishvelayutham@Jegadishs-MacBook-Air.local>
2025-10-02 10:29:04 +0530

    added files

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,11 +1,14 @@
 
 resource "aws_instance" "web" {
-  name                   = var.name
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = var.security_group_ids
 
   associate_public_ip_address = true
-  tags = var.tags
+  tags = merge(
+    {
+      Name = var.name   
+    }
+  )
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  description = "Name of the Instance"
+  description = "Name of the Security Group"
   type        = string
   default     = "app-ec2"
 }
@@ -21,10 +21,4 @@ variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
   default     = "ami-0dee22c13ea7a9a67"  
-}
-
-variable "tags" {
-  description = "Tags to apply to the instance"
-  type        = map(string)
-  default     = {}
 }
