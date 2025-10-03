4e50e33f6f41441f3acf14ba78e5c8dcb91d8474
sanjay812 <72460414+sanjay812@users.noreply.github.com>
2025-10-03 16:42:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -99,8 +99,14 @@ ssh -i ~/.ssh/id_rsa ubuntu@"$EC2_IP" << 'EOF'
     cd /home/ubuntu/deployments
     sudo mkdir -p ./loki-data
     sudo chown -R 10001:10001 ./loki-data
-    sudo docker compose pull
-    sudo docker compose up -d
+    if [ -z "$(sudo docker ps -q)" ]; then
+      echo "No containers running → First deployment"
+      sudo docker compose pull
+      sudo docker compose up -d
+    else
+      echo "Containers already running → Update only app"
+      sudo docker compose up -d --pull always api
+    fi
 
 
 EOF
