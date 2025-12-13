6c12239f10e9e08c76b7ef94f969b333b4eaac66
sanjay812 <72460414+sanjay812@users.noreply.github.com>
2025-12-13 16:16:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "my-terraform-state-bucket-7595e7a6" 
-    key     = "terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
+    bucket         = "terraform-state-17014c2b"
+    key            = "test/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock-17014c2b"
+    encrypt        = true
   }
 }
