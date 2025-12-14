a6731bf3c211e8c0a5349a1ad868eabb312f8053
sanjay812 <sanjaychinnikarthick@gmail.com>
2025-12-14 13:39:13 +0530

    vpc

--- main.tf
+++ main.tf
@@ -30,4 +30,7 @@ module "networking" {
   source = "./modules/networking"
   vpc_cidr = var.vpc_cidr
   vpc_name = var.vpc_name
+  cidr_private_subnet = var.cidr_private_subnet
+  cidr_public_subnet = var.cidr_public_subnet
+  ap_availability_zone = var.ap_availability_zone
 }
